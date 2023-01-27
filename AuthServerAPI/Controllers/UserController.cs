420b3f4c63cf5d79ae76a9c18a7a1a758c9927a8
Selcuk ARSLAN <slckarslan93@gmail.com>
2023-01-27 16:25:10 +0300

    UserController

---
+++ AuthServerAPI/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using AuthServer.Core.Dtos;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AuthServerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : CustomBaseController
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        //api/user
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserDto createUserDto)
+        {
+            return ActionResulInstance(await _userService.CreateUserAsync(createUserDto));
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            return ActionResulInstance(await _userService.GetUserByNameAsync(HttpContext.User.Identity.Name));
+        }
+    }
+}
--- AuthServerAPI/Startup.cs
+++ AuthServerAPI/Startup.cs
@@ -98,7 +98,7 @@ namespace AuthServerAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
