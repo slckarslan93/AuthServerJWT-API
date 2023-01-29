c4f119d52dbcd07c6166713a1c6cdc0526da364a
Selçuk ARSLAN <slckarslan93@gmail.com>
2023-01-29 13:26:54 +0300

    MiniApi

---
+++ MiniApp2.API/Controllers/InvoiceController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Security.Claims;
+
+namespace MiniApp2.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+        public IActionResult GetInvoices()
+        {
+            var userName = HttpContext.User.Identity.Name;  
+            var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+            return Ok($"Stok işlemleri => UserName:{userName}-UserId:{userId}");
+        }
+    }
+}
